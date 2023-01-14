a0ac87e24be6e14c97986a03a317a3995c25514c
Natacha Vergara <ntchvergara@gmail.com>
2023-01-14 12:34:30 -0300

    Creacion de la carpeta Route y transpasado las rutas hechas en el archivo server.js

--- Routes/authorsRoute.js
+++ Routes/authorsRoute.js
@@ -1,8 +1,18 @@
-const expres = require('express')
-const route = expres.Router()
+const express = require('express')
+const route = express.Router()
 
 
-route.get('/', (req, res)=>{
-    res.send('Todos los libros')
-}) 
+route.get('/', (req, res) => res.send('Obtención de autores'));
+route.get('/:id', (req, res) => res.send(`Obtención de un autor con id ${req.params.id}`));
 
+route.post('/create', (req, res) => res.send('Creación de un autor'));
+route.post('/create/:id', (req, res) => res.send(`Creación de un autor con id ${req.params.id}`));
+
+route.put('/update', (req, res) => res.send('Actualización de un autor'));
+route.put('/update/:id', (req, res) => res.send(`Actualizacion de un autor con id ${req.params.id}`));
+
+route.delete('/delete', (req, res) => res.send('Eliminación de un autor'));
+route.delete('/delete/:id', (req, res) => res.send(`Eliminación de un autor con id ${req.params.id}`));
+
+
+module.exports = route;
--- server.js
+++ server.js
@@ -2,24 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
+const authorsRoute = require('./routes/authorsRoute');
 
 
-
-//app.use('/', (req, res) => res.send('Hola mundo'));
-
-
-app.get('/authors', (req, res) => res.send('Obtención de autores'));
-app.get('/authors/:id', (req, res) => res.send(`Obtención de un autor con id ${req.params.id}`));
-
-app.post('/authors/create', (req, res) => res.send('Creación de un autor'));
-app.post('/authors/create/:id', (req, res) => res.send(`Creación de un autor con id ${req.params.id}`));
-
-app.put('/authors/update', (req, res) => res.send('Actualización de un autor'));
-app.put('/authors/update/:id', (req, res) => res.send(`Actualizacion de un autor con id ${req.params.id}`));
-
-app.delete('/authors/delete', (req, res) => res.send('Eliminación de un autor'));
-app.delete('/authors/delete/:id', (req, res) => res.send(`Eliminación de un autor con id ${req.params.id}`));
-
+app.get('/', (req, res) => res.send('Hola mundo'))
+app.use('/authors', authorsRoute);
 
 
 
