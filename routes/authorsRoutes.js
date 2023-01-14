71b7e63efadd7e3d1af325232350a7246b02b067
Natacha Vergara <ntchvergara@gmail.com>
2023-01-14 18:00:56 -0300

    terminado la clase Rutas con express

--- routes/authorsRoutes.js
+++ routes/authorsRoutes.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const route = express.Router()
 
+route.use((req,res,next)=>{
+    const {host} = req.headers;
+    res.host = host;
+    next()
+})
+
 
 route.get('/', (req, res) => res.send('Obtención de autores'));
-route.get('/:id', (req, res) => res.send(`Obtención de un autor con id ${req.params.id}`));
+//middleware
+route.get('/:id',
+    (req, res, next) => {
+        const { id } = req.params;
+        const numId = Number(id);
+
+        if (isNaN(numId) || numId < 1) {
+            return res.send('El id es inválido');
+        } else {
+            next();
+        }
+    },
+    (req, res) => res.send(`Obtención de un autor con id ${req.params.id} para el host ${res.host}`)
+);
 
 route.post('/create', (req, res) => res.send('Creación de un autor'));
 route.post('/create/:id', (req, res) => res.send(`Creación de un autor con id ${req.params.id}`));
@@ -13,6 +32,5 @@ route.put('/update/:id', (req, res) => res.send(`Actualizacion de un autor con i
 
 route.delete('/delete', (req, res) => res.send('Eliminación de un autor'));
 route.delete('/delete/:id', (req, res) => res.send(`Eliminación de un autor con id ${req.params.id}`));
-
 
 module.exports = route;
