00153fef3ca349963c5ca3336af9ba2226f8ffc8
Natacha Vergara <ntchvergara@gmail.com>
2023-01-16 10:29:53 -0300

    Agregue cors al archivo server

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = 3001;
+
+
+
 
 const authorsRoute = require('./routes/authorsRoutes');
 
+
 app.use('/static', express.static(__dirname + '/public'));
+app.use(cors());
 app.get('/', (req, res) => res.send('Hola mundo'));
 app.use('/authors', authorsRoute);
 
