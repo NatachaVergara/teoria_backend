ffd4fae630fad6f080450f1164eba28ada5edae4
Natacha Vergara <ntchvergara@gmail.com>
2023-01-21 14:11:17 -0300

    asd

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -4,7 +4,6 @@ module.exports.createUserController = async (req, res) => {
     const { email, password } = req.body;
     try {
         const data = await createUser(email, password);
-
         return data.created ? res.status(200).send(data) : res.status(200).send(data)
     } catch (error) {
         console.log(error)
--- models/usersModel.js
+++ models/usersModel.js
@@ -1,32 +1,15 @@
 const { request } = require('../db/request');
 
 module.exports.createUser = async (email, password) => {
-    const exist = await request(`SELECT * FROM user WHERE email = "${email}"`)
+    const data = await request(`INSERT INTO users(email, password, type) VALUES("${email}", "${password}", "NOTADMIN")`)
 
-    if (exist.length !== 0) {
-        return { msg: 'Este email ya se encuentra registrado' }
-    } else {
-        const data = await request(`INSERT INTO users(email, password, type) VALUES("${email}", "${password}", "NOTADMIN")`)
-
-        return data.insertId ? { created: true, msg: 'Usuario creado, ahora puede loguearse' } : { created: false, msg: 'Hubo un problema y no se pudo crear el usuario. Intente mas tarde' }
-    }
-
+    return data.insertId ? { created: true, msg: 'Usuario creado, ahora puede loguearse' } : { created: false, msg: 'Hubo un problema y no se pudo crear el usuario. Intente mas tarde' }
 
 }
 
 module.exports.createAdmin = async (email, password) => {
+    const data = await request(`INSERT INTO users(email, password, type) VALUES("${email}", "${password}", "ADMIN")`)
 
-    const exist = await request(`SELECT * FROM user WHERE email = "${email}" `)
-
-    if (exist.length !== 0) {
-        return { msg: 'Este email ya se encuentra registrado' }
-    } else {
-        const data = await request(`INSERT INTO users(email, password, type) VALUES("${email}", "${password}", "ADMIN")`)
-
-        return data.insertId ? { created: true, msg: 'Admin creado satisfactoriamente', query: data } : { created: false, msg: 'Hubo un problema y no se pudo crear el usuario. Intente mas tarde', query: data }
-    }
-
-
-
+    return data.insertId ? { created: true, msg: 'Admin creado satisfactoriamente', query: data } : { created: false, msg: 'Hubo un problema y no se pudo crear el usuario. Intente mas tarde', query: data }
 
 }
