b7c99764c81371151bef5f6990830a4ed4132776
Natacha Vergara <ntchvergara@gmail.com>
2023-01-21 13:47:18 -0300

    tenia un error en el controlador

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,4 +1,4 @@
-const { createUser } = require("../models/usersModel")
+const { createUser, createAdmin } = require("../models/usersModel")
 
 module.exports.createUserController = async (req, res) => {
     const { email, password } = req.body;
@@ -14,7 +14,7 @@ module.exports.createUserController = async (req, res) => {
 module.exports.createAdminController = async (req, res) => {
     const { email, password } = req.body;
     try {
-        const data = await createUser(email, password);
+        const data = await createAdmin(email, password);
         return data.created ? res.status(200).send(data) : res.status(200).send(data)
     } catch (error) {
         console.log(error)
--- models/usersModel.js
+++ models/usersModel.js
@@ -1,7 +1,7 @@
 const { request } = require('../db/request');
 
 module.exports.createUser = async (email, password) => {
-    const data = await request(`INSERT INTO users(email,password)VALUES("${email}", "${password}", "NOTADMIN")`)
+    const data = await request(`INSERT INTO users(email, password) VALUES("${email}", "${password}", "NOTADMIN")`)
 
     return data.insertId ? { created: true, msg: 'Usuario creado, ahora puede loguearse' } : { created: false, msg: 'Hubo un problema y no se pudo crear el usuario. Intente mas tarde' }
 
