15793bf480852238ab993c514c7b70104ffce7c5
Natacha Vergara <ntchvergara@gmail.com>
2023-01-21 14:03:55 -0300

    actualizando el modelo y controlador

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -4,6 +4,7 @@ module.exports.createUserController = async (req, res) => {
     const { email, password } = req.body;
     try {
         const data = await createUser(email, password);
+
         return data.created ? res.status(200).send(data) : res.status(200).send(data)
     } catch (error) {
         console.log(error)
--- models/usersModel.js
+++ models/usersModel.js
@@ -1,15 +1,32 @@
 const { request } = require('../db/request');
 
 module.exports.createUser = async (email, password) => {
-    const data = await request(`INSERT INTO users(email, password, type) VALUES("${email}", "${password}", "NOTADMIN")`)
+    const exist = await request(`SELECT * FROM user WHERE email = "${email}"`)
 
-    return data.insertId ? { created: true, msg: 'Usuario creado, ahora puede loguearse' } : { created: false, msg: 'Hubo un problema y no se pudo crear el usuario. Intente mas tarde' }
+    if (exist.length !== 0) {
+        return { msg: 'Este email ya se encuentra registrado' }
+    } else {
+        const data = await request(`INSERT INTO users(email, password, type) VALUES("${email}", "${password}", "NOTADMIN")`)
+
+        return data.insertId ? { created: true, msg: 'Usuario creado, ahora puede loguearse' } : { created: false, msg: 'Hubo un problema y no se pudo crear el usuario. Intente mas tarde' }
+    }
+
 
 }
 
 module.exports.createAdmin = async (email, password) => {
-    const data = await request(`INSERT INTO users(email, password, type) VALUES("${email}", "${password}", "ADMIN")`)
 
-    return data.insertId ? { created: true, msg: 'Admin creado satisfactoriamente', query:data } : { created: false, msg: 'Hubo un problema y no se pudo crear el usuario. Intente mas tarde', query:data }
+    const exist = await request(`SELECT * FROM user WHERE email = "${email}" `)
+
+    if (exist.length !== 0) {
+        return { msg: 'Este email ya se encuentra registrado' }
+    } else {
+        const data = await request(`INSERT INTO users(email, password, type) VALUES("${email}", "${password}", "ADMIN")`)
+
+        return data.insertId ? { created: true, msg: 'Admin creado satisfactoriamente', query: data } : { created: false, msg: 'Hubo un problema y no se pudo crear el usuario. Intente mas tarde', query: data }
+    }
+
+
+
 
 }
