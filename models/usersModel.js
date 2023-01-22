7006085528ed1a028797d86fe0e6c7deb33fafe6
Natacha Vergara <ntchvergara@gmail.com>
2023-01-22 11:14:51 -0300

    actualice el modelo para el login

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,12 +1,12 @@
 const { request } = require('../db/request');
 
-const {hashPassword} = require('../utils/password')
+const { hashPassword, comparePassword } = require('../utils/password')
 
 module.exports.createUser = async (email, password) => {
     const hashedPassword = hashPassword(password)
     const data = await request(`INSERT INTO users(email, password, type) VALUES("${email}", "${hashedPassword}", "NOTADMIN")`);
 
-    return data.insertId ? { created: true, msg: 'Usuario creado, ahora puede loguearse', query: data  } : { created: false, msg: 'Hubo un problema y no se pudo crear el usuario. Intente mas tarde',  query: data  }
+    return data.insertId ? { created: true, msg: 'Usuario creado, ahora puede loguearse', query: data } : { created: false, msg: 'Hubo un problema y no se pudo crear el usuario. Intente mas tarde', query: data }
 
 };
 
@@ -21,5 +21,10 @@ module.exports.createAdmin = async (email, password) => {
 module.exports.loginUser = async (email, password) => {
     const data = await request(`SELECT * FROM users WHERE email = "${email}"`);
 
-    return data.length ? { isUser: true, query: data[0], msg: 'Bienvenido/a' } : { isUser: false, msg: 'Email y/o contraseña equivocada. ¿Ya tiene cuenta?' }
+    if (data.length && comparePassword(password, data[0].password)) {
+        delete data[0].password;
+        return { isUser: true, query: data[0], msg: 'Bienvenido/a' }
+    } else {
+        return { isUser: false, msg: 'Email y/o contraseña equivocada. ¿Ya tiene cuenta?' }
+    }
 };
