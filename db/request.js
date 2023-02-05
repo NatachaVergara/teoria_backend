c2e6734a30041c1bb2b636c546fe206d51123195
Natacha Vergara <natacha.vergara@fuzzyfish.com>
2023-02-05 17:03:27 -0300

    modulo .env

--- db/request.js
+++ db/request.js
@@ -1,12 +1,16 @@
 const mysql = require('mysql');
+require('dotenv').config()
+
+
+
 
 module.exports.request = (query) => new Promise((res, rej) => {
     const connection = mysql.createConnection({
-        host: 'mysql-websitebackend.alwaysdata.net',
-        port: 3306,
-        user: '296653_backenddb',
-        password: 'Elisabet1986!',
-        database: 'websitebackend_db'
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
     });
 
     connection.query(query, (error, data, fields) => {
